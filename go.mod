de1f203d28a892e1b25fe5a8972711666a9cf498
Shandy Hidayat <hidayat.shandy@gmail.com>
2020-09-08 01:19:46 +0700

    setup gorilla mux and web server

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/go-toggle
+
+go 1.14
+
+require github.com/gorilla/mux v1.8.0
+//require github.com/jackc/pgx v4.8.1
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
+func main() {
+	fmt.Println("Welcome to go-toggle")
+	router := mux.NewRouter()
+	router.HandleFunc("/posts", getPost).Methods("GET")
+
+	http.ListenAndServe(":8000", router)
+}
+
+type Post struct {
+	ID    string
+	Title string
+	Body  string
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var post1 Post
+	post1.ID = "12345"
+	post1.Title = "Welcome to go-toggle"
+	post1.Body = "Welcome to go-toggle"
+	json.NewEncoder(w).Encode(post1)
 }
